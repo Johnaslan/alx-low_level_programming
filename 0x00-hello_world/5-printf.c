55641059a075881b1353ce724750ea400fbd6623
Johnaslan <johnaslan35@gmail.com>
2022-09-08 11:06:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print using printf
+*
+* Return: 0
+*
+*/
+
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a peice of art, \n");
+	return (0);
+}
