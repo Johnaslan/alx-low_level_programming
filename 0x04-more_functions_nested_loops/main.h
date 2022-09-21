ed60db17e44cff36ef8df47570c8907f0bcad300
Johnaslan <johnaslan35@gmail.com>
2022-09-20 21:41:34 -0700

    1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,16 +5,8 @@
   * _putchar - puts character to standard output 
   * @c: character to put to standard output 
   */ 
-  
- int _putchar(char c); 
-  
- /** 
-  * _isupper - checks for uppercase character 
-  * @c: character to be checked 
-  * Return: 1 if true, 0 if false 
-  */ 
-  
- int _isupper(int c); 
+
+int _isupper(int c); 
   
  /** 
   * _isdigit - checks for a digit 0 through 9 
